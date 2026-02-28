eecd7393fb9fe8196b0edaf796942119bc7d9a73
keryanr-eng <keryanr@gmail.com>
2026-02-28 14:46:41 +0100

    fix: use hash router for stable github pages refresh

---
+++ src/data/frenchWords.ts
@@ -0,0 +1,9 @@
+export const FRENCH_WORDS = [
+  'ami','amie','arbre','avion','banane','bateau','beau','belle','bonjour','bureau','cabane','cahier','carte',
+  'chat','chien','classe','clavier','code','complet','cuisine','danse','debut','ecole','ecrire','eleve','etoile',
+  'famille','fenetre','fleur','fromage','garage','guitare','heureux','histoire','image','joueur','journal','lampe',
+  'lettre','livre','maison','manger','marine','matin','mercredi','mobile','montagne','musique','nombre','nuage',
+  'orange','ordinateur','pantalon','papier','parler','partie','piano','plage','poisson','portable','pratique',
+  'question','rapide','reponse','route','saison','salade','science','soleil','stylo','tableau','telephone','temps',
+  'tortue','train','valise','velo','voyage','voiture','zebre'
+];
